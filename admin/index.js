58253fa92cf695d2729461371f11683f499a3966
Ram Dulare <v-ramdulare@microsoft.com>
2023-11-08 12:08:15 +0530

    admin add

---
+++ admin/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter', (req, res)=>{
+    res.send('rdgupta0406')
+})
+app.get('/hello', (req, res)=>{
+    res.send('Hello Admin')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ snapgram/src/main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { ReactDOM } from "react";
+import App from "./App";
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <App>
+    
+  </App>
+)
